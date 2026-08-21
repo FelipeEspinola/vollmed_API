57ff35d102bd2f7e2f990bc64c84e84df6ad9d43
FelipeEspinola <fmespinola00@gmail.com>
2026-08-21 11:36:58 -0300

    Enviando versão final do dia 21/06

--- src/main/java/br/com/sistema/api/controller/OlaController.java
+++ src/main/java/br/com/sistema/api/controller/OlaController.java
@@ -1,5 +1,15 @@
 package br.com.sistema.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // Registra a classe OlaController como um controller do SpringBoot
+@RequestMapping("ola") // Define a url base para todos os metodos dessa classe
 public class OlaController {
-    
+
+    @GetMapping("/olamundo") // Método http do tipo GET
+    public String olaMundo(){
+        return "Olá Mundo!!!";
+    }
 }
--- src/main/java/br/com/sistema/api/controller/PacienteController.java
+++ src/main/java/br/com/sistema/api/controller/PacienteController.java
@@ -1,5 +1,28 @@
 package br.com.sistema.api.controller;
 
-public class PacienteController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("paciente") 
+public class PacienteController { 
+    //GET Request -> Response -> EX: Tela home
+
+    @GetMapping("/") //Aponta para Localhost:8080/paciente
+    public String exibirHome() {
+        return "a";
+    }
     
+
+    //GET/POST Request -> Response -> Ex: Cadastrar Paciente. Exibe tela de cadastro e o POST é chamado quando o enviar é clicado.
+    //GET/PUT Request -> Response -> Ex: Alterar telefone. Get exibe a tela de alteração e o PUT é chamado quando o botão alterar é clicado.
+    
+    //DELETE
+    
+    // CRUD 
 }
+
+
